df3b6717cc102630a8ddf678e1fc2164e69df28e
Artem Cherenkov <art.cherenkov@gmail.com>
2021-03-26 01:58:56 +0700

    feat: add useTypedSelector hook

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
+import { shallowEqual } from "react-redux";
+
 import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories,
+    shallowEqual
+  );
 
   const onChange = (evt: React.ChangeEvent<HTMLInputElement>) =>
     setTerm(evt.target.value);
